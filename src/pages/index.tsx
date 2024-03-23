78104e772a25dc0af023bc29911c338b677f457f
Murilloxfogaca <sxecesar@gmail.com>
2024-03-23 13:36:23 -0300

    :gear:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { NextSeo } from "next-seo";
 import Main from "@/components/Main";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import Head from "next/head";
 
 export default function Home() {
   return (
@@ -11,6 +12,13 @@ export default function Home() {
         title="Home: Atividades de Extensão"
         description="Welcome to Atividades de Extensão homepage."
       />
+      <Head>
+        <title>Proteja-se | Atividades de Extensão</title>
+        <meta
+          name="description"
+          content="Atividades de Extensão: Integração de Competências em Análise e Desenvolvimento de Sistemas I - Turma_001"
+        ></meta>
+      </Head>
       <Header />
       <Main />
       <Footer />
