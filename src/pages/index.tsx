f530ea5271fbc01ae8744c43776ae046886e6864
Murilloxfogaca <sxecesar@gmail.com>
2024-03-23 13:42:55 -0300

    :gear:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,12 @@
 import React from "react";
-import { NextSeo } from "next-seo";
 import Main from "@/components/Main";
 import Footer from "@/components/Footer";
-import Header from "@/components/Header";
 import Head from "next/head";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <div className="text-white">
-      <NextSeo
-        title="Home: Atividades de Extensão"
-        description="Welcome to Atividades de Extensão homepage."
-      />
       <Head>
         <title>Proteja-se | Atividades de Extensão</title>
         <meta
